3fc19e1545af9ae1a441e1328b818a865620dd1f
FrancoLelli <franlelli1966@gmail.com>
2022-08-04 20:53:01 -0300

    Segundo commit

--- js/js.js
+++ js/js.js
@@ -5,7 +5,11 @@ alert("Luego presiona en agregar, y tendras tu nota guardada")
 const fecha = Date.now();
 const hoy = new Date(fecha);
 const fechaActual = hoy.toLocaleDateString()
+
 notas=[]
+
+const botonAgregar = document.getElementById("btnAgregar")
+
 class Nota{
     constructor(titulo, contenido, fecha){
         titulo = this.titulo
@@ -22,23 +26,22 @@ function crearNota(){
         contenido: contenidoNota, 
         fecha: fechaActual
     })
-    notas.forEach((cadaUna) => {
-        const cardNotas = document.getElementById("notas");
-        const divNotas = document.createElement('div');
-        divNotas.className = "card mb-3";
-        divNotas.innerHTML = `<div id="nuevoElemento " class="card-header">
-                                ${cadaUna.titulo}
-                                </div>
-                                <div class="card-body">
-                                <blockquote class="blockquote mb-0">
-                                    <p>${cadaUna.contenido}</p>
-                                    <footer class="blockquote-footer">Fecha de hoy: <cite title="">${cadaUna.fecha}</cite></footer>
-                                </blockquote>
-                                </div>
-                                <div class="boton  blur-in">
-                                    <button type="button" class="btn btn-danger" onclick="">Eliminar</button>
-                                </div>`
-        cardNotas.append(divNotas)
-    });
+    const cardNotas = document.getElementById("notas");
+    const divNotas = document.createElement('div');
+    divNotas.className = "card mb-3";
+    divNotas.innerHTML = `<div id="nuevoElemento " class="card-header">
+                            ${tituloNota}
+                            </div>
+                            <div class="card-body">
+                            <blockquote class="blockquote mb-0">
+                                <p>${contenidoNota}</p>
+                                <footer class="blockquote-footer">Fecha: <cite title="">${fechaActual}</cite></footer>
+                            </blockquote>
+                            </div>
+                            <div class="boton  blur-in">
+                                <button type="button" class="btn btn-danger" onclick="">Eliminar</button>
+                            </div>`
+    cardNotas.append(divNotas)
 }
 
+botonAgregar.addEventListener('click', crearNota)
