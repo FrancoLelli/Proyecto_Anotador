27097834a00471955f8db56df6229857291e050e
FrancoLelli <franlelli1966@gmail.com>
2022-08-15 20:51:52 -0300

    cambios varios

--- js/js.js
+++ js/js.js
@@ -8,28 +8,43 @@ const fechaActual = hoy.toLocaleDateString()
 
 notas=[]
 
+let id= 1
+
 const botonAgregar = document.getElementById("btnAgregar")
+let usuarios = JSON.parse(localStorage.getItem('usuarios'));
+
+const botonEliminar = document.getElementById("eliminarBtn")
+const modalContainer = document.querySelector('#modal-container')
+const notaModal = document.getElementById('eliminarNota')
+const cerrarModal = document.getElementById('cerrarModal')
+
+
 
 class Nota{
-    constructor(titulo, contenido, fecha){
+    constructor(id,titulo, contenido, fecha){
+        id= this.id
         titulo = this.titulo
         contenido = this.contenido
         fecha = fechaActual
     }
 }
 
+
 function crearNota(){
+    id++
     tituloNota = document.getElementById("tituloNota").value
     contenidoNota = document.getElementById("contenidoNota").value
     notas.push({
+        id: id,
         titulo: tituloNota, 
         contenido: contenidoNota, 
         fecha: fechaActual
     })
     const cardNotas = document.getElementById("notas");
     const divNotas = document.createElement('div');
-    divNotas.className = "card mb-3";
-    divNotas.innerHTML = `<div id="nuevoElemento " class="card-header">
+    divNotas.setAttribute('id', `${id}`)
+    divNotas.className = "pachorra card mb-3";
+    divNotas.innerHTML = `<div class="card-header">
                             ${tituloNota}
                             </div>
                             <div class="card-body">
@@ -37,15 +52,31 @@ function crearNota(){
                                 <p>${contenidoNota}</p>
                                 <footer class="blockquote-footer">Fecha: <cite title="">${fechaActual}</cite></footer>
                             </blockquote>
-                            </div>
-                            <div class="boton  blur-in">
-                                <button type="button" class="btn btn-danger" onclick="">Eliminar</button>
                             </div>`
+
+    const botonRemove = document.createElement('div')
+    botonRemove.classList.add('boton', 'blur-in')
+    botonRemove.innerHTML = `<button type="button" class="btn btn-sm btn-danger" id="${id}">Eliminar</button>`
+    divNotas.appendChild(botonRemove)
+    botonRemove.onclick = function(){
+        modalContainer.classList.add('my-modal-activo');
+        cerrarModal.onclick = () =>{
+            modalContainer.classList.remove('my-modal-activo')
+        }
+        notaModal.onclick = () =>{
+            modalContainer.classList.remove('my-modal-activo')
+            eliminarNota(divNotas.getAttribute('id'))
+        }
+    }
+
     cardNotas.append(divNotas)
 }
 
 botonAgregar.addEventListener('click', crearNota)
 
-function eliminarNota(){
-    
+
+function eliminarNota(id){
+    document.getElementById(id).remove()
 }
+
+
--- js/js2.js
+++ js/js2.js
@@ -1,23 +1,66 @@
 
 const botonIniciar = document.getElementById("btnIniciar")
+const botonCrear = document.getElementById("btnCrear")
+const modalContainer = document.querySelector('#modal-container')
+modalContainer.classList.add('my-modal-activo');
+const cerrarModal = document.getElementById('entendidoBtn')
+cerrarModal.onclick = () =>{
+    modalContainer.classList.remove('my-modal-activo');
+}
+class Persona{
+    constructor(nombre, contrasenia){
+        nombre = this.nombre
+        contrasenia = this.contrasenia
+    }
+}
+
+function returnLista(){
+    let usuarios = localStorage.getItem('usuarios');
+    if(!usuarios){
+        usuarios = []
+    }else{
+        usuarios = JSON.parse(localStorage.getItem('usuarios'));
+    }
+    console.log(usuarios);
+    return usuarios
+}
+
+function agregar_A_Lista(){
+    let lista = returnLista()
+    lista.push(
+        {
+            nombre: document.getElementById("email").value,
+            contrasenia: document.getElementById("password").value
+        }
+    ); 
+
+    localStorage.setItem('usuarios', JSON.stringify(lista))
+    console.log(lista);
+}
+
 
 function validar(){
+    let usuariosLocal = JSON.parse(localStorage.getItem('usuarios'))
+
     const inputEmail = document.getElementById("email")
     const inputPassword = document.getElementById("password")
     const posibleError = document.getElementById("posibleError")
-    const email = document.getElementById("email").value
+    const nombre = document.getElementById("email").value
     const password = document.getElementById("password").value
 
-    if(email !== "prueba" || password !== "prueba"){
+    const recorrertData = usuariosLocal.some(usuario => nombre === usuario.nombre && password === usuario.contrasenia)
+
+    if(recorrertData){
+        posibleError.classList.add("errorEscondido")
+        posibleError.classList.remove("errorPosible")
+        location.href = "./menu.html"
+    }else{
         inputEmail.classList.add("error")
         inputPassword.classList.add("error")
         posibleError.classList.remove("errorEscondido")
         posibleError.classList.add("errorPosible")
-    }else{
-        posibleError.classList.add("errorEscondido")
-        posibleError.classList.remove("errorPosible")
-        location.href = "./menu.html"
     }
+
 }
 
 document.addEventListener('keypress', (e) =>{
@@ -25,4 +68,6 @@ document.addEventListener('keypress', (e) =>{
         validar()
     }
 })
-botonIniciar.addEventListener('click', validar)
+
+botonCrear.addEventListener('click', agregar_A_Lista) 
+botonIniciar.addEventListener('click', validar) 
